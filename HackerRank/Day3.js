55fffeda8192e97cfa5774b5719be0db96852d73
Anna Kawecka <aakawecka89@gmail.com>
2023-03-10 21:02:36 +0100

    part 2 of day3

--- HackerRank/Day3.js
+++ HackerRank/Day3.js
@@ -21,3 +21,18 @@ function getSecondLargest(nums) {
 
   return res[0];
 }
+
+//part2
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+  try {
+    s = s.split("").reverse().join("");
+  } catch (e) {
+    console.log("s.split is not a function");
+  }
+  console.log(s);
+}
