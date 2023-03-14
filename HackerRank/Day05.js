582f26c0db0f2151d604eeb16e788b7503b822ad
Anna Kawecka <aakawecka89@gmail.com>
2023-03-14 22:30:15 +0100

    10 days of js p3

--- HackerRank/Day05.js
+++ HackerRank/Day05.js
@@ -52,3 +52,23 @@ function sides(literals, ...expressions) {
   const value = Math.sqrt(perimeter ** 2 - 16 * area);
   return [(perimeter - value) / 4, (perimeter + value) / 4];
 }
+
+//p3
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ *
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+  const newArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      newArr.push(nums[i] * 2);
+    } else {
+      newArr.push(nums[i] * 3);
+    }
+  }
+  return newArr;
+}
