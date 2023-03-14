fc6590d5f4a921ff7ad0ef24edf6e9c41147b3d8
Anna Kawecka <aakawecka89@gmail.com>
2023-03-14 20:46:44 +0100

    10 days of js p2

--- HackerRank/Day05.js
+++ HackerRank/Day05.js
@@ -34,3 +34,21 @@ function regexVar() {
    */
   return re;
 }
+
+//p2
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ *
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+  const [area, perimeter] = expressions;
+
+  const value = Math.sqrt(perimeter ** 2 - 16 * area);
+  return [(perimeter - value) / 4, (perimeter + value) / 4];
+}
