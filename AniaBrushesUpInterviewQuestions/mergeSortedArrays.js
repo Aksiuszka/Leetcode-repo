49b7865402b365b67efd6267469592a4159eb996
Anna Kawecka <aakawecka89@gmail.com>
2023-07-10 23:41:44 +0200

    leetcode merge

--- AniaBrushesUpInterviewQuestions/mergeSortedArrays.js
+++ AniaBrushesUpInterviewQuestions/mergeSortedArrays.js
@@ -19,3 +19,22 @@ const merge = (arrayList) => {
   return arrayList.flat(Infinity).sort((a, b) => a - b);
 };
 merge(array);
+
+//task from leetcode. Easy
+
+var merge2 = function (nums1, m, nums2, n) {
+  const getProperArr = (arr, n) => {
+    let newArr = [];
+    for (let i = 0; i < n; i++) {
+      newArr.push(arr[i]);
+    }
+    return newArr;
+  };
+  const newNums1 = getProperArr(nums1, m);
+
+  const newNums2 = getProperArr(nums2, n);
+
+  const arrayList = newNums1.concat(newNums2).sort((a, b) => a - b);
+  console.log(arrayList);
+  return arrayList;
+};
